233e3e613ad09b9f2c1fc0508a77807a5ea6e56d
jarvis <nvdung149@gmail.com>
2021-05-19 16:47:15 +0700

    remove utils

--- contracts/wrappers/FetchTokenBalances.sol
+++ contracts/wrappers/FetchTokenBalances.sol
@@ -2,10 +2,13 @@ pragma solidity 0.7.6;
 
 import "@kyber.network/utils-sc/contracts/Withdrawable.sol";
 import "@kyber.network/utils-sc/contracts/IBEP20.sol";
-import "@kyber.network/utils-sc/contracts/Utils.sol";
 
-contract FetchTokenBalances is Utils, Withdrawable {
+contract FetchTokenBalances is Withdrawable {
     constructor(address _admin) Withdrawable(_admin) {}
+
+    IBEP20 internal constant BNB_TOKEN_ADDRESS = IBEP20(
+        0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB
+    );
 
     function getBalances(address account, IBEP20[] calldata tokens)
         external
