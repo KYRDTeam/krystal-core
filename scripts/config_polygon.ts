9f9ff4d1344549df5cc34c5f5ab7fd10766c1f4c
Jarvis <jarvis57@users.noreply.github.com>
2021-08-13 16:04:50 +0700

    ape on polygon (#28)

--- scripts/config_polygon.ts
+++ scripts/config_polygon.ts
@@ -20,6 +20,7 @@ export const PolygonConfig: Record<string, IConfig> = {
       routers: [
         '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff', // quickswap
         '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', // sushiswap
+        '0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607', // apeswap
       ],
     },
 
