6af55c5888d7a7045670931c153b24daab110791
Vinh Nguyen <vinhnd.ict@gmail.com>
2022-04-27 17:55:47 +0700

    Deploy KyberSwapV2 on Aurora

--- scripts/config_aurora.ts
+++ scripts/config_aurora.ts
@@ -8,6 +8,12 @@ export const AuroraConfig: Record<string, IConfig> = {
     tokens: {},
 
     wNative: '0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB',
+
+    kyberSwapV2: {
+      router: '0xdf1a1b60f2d438842916c0adc43748768353ec25',
+      aggregationExecutor: '0xd12bcdfb9a39be79da3bdf02557efdcd5ca59e77',
+    },
+
     supportedWallets: commonPlatformWallets,
     nativeUsdRate: 1,
   },
