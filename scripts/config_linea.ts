dc7c7dc18aba100c98c7ef31b391e8d50a3694c0
phanydi <phanydi@gmail.com>
2023-07-13 13:34:56 +0700

    add kyberv3 + univ2 on linea

--- scripts/config_linea.ts
+++ scripts/config_linea.ts
@@ -10,6 +10,19 @@ export const LineaConfig: Record<string, IConfig> = {
       router: '0x6352a56caadc4f1e25cd6c75970fa768a3304e64',
     },
 
+    kyberSwapV3: {
+      router: '0xcd9478E0533F6108A036389A4C24E11f191B54D8',
+    },
+
+    // Uniswap & clones
+    uniswap: {
+      routers: {
+        univ2: {
+          address: '0x0913482dccc4ccf7219897e0976d3a2727f4316c',
+        },
+      },
+    },
+
     supportedWallets: commonPlatformWallets,
     nativeUsdRate: 70,
 
