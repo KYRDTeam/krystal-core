e418c085bdaff617bb55cd81b703845dea60bc7f
phanydi <phanydi@gmail.com>
2023-07-14 11:25:12 +0700

    change swap contract for NooBys

--- scripts/config_linea.ts
+++ scripts/config_linea.ts
@@ -4,7 +4,8 @@ export const LineaConfig: Record<string, IConfig> = {
   linea_goerli: {
     autoVerifyContract: true,
     tokens: {},
-    wNative: '0x2C1b868d6596a18e32E61B901E4060C872647b6C',
+    //remember to check if this compatible w/ weth that dex used
+    wNative: '0x62c70f9379ac0470da3149d0e1c90d07f313e3dc',
 
     openOcean: {
       router: '0x6352a56caadc4f1e25cd6c75970fa768a3304e64',
